34a74a0ec59a69fbf8615f3a5484237a5c14f137
Jay <jvelayud@gmail.com>
2021-08-20 14:29:59 -0400

    Adding some faulty code

--- src/get.js
+++ src/get.js
@@ -1,18 +1,20 @@
 import handler from "./util/handler";
 import dynamoDb from "./util/dynamodb";
 
-export const main = handler(async (event) => {
+export const main = handler(async (event, context) => {
   const params = {
-    TableName: process.env.TABLE_NAME,
+    TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      noteId: event.pathParameters.id, // The id of the note from the path
-    },
+      userId: event.requestContext.identity.cognitoIdentityId,
+      // noteId: event.pathParameters.id
+    }
   };
 
   const result = await dynamoDb.get(params);
-  if (!result.Item) {
+  if ( ! result.Item) {
     throw new Error("Item not found.");
   }
 
